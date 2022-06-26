bc27f8cd96082372473d1794e192c27d9ee4753c
Shun <47809066+saitoshi@users.noreply.github.com>
2022-06-27 06:33:55 +0900

    Update 3JSToU3D.js

--- js/3JSToU3D.js
+++ js/3JSToU3D.js
@@ -2,3 +2,14 @@
 var canvas;
 
 var scenes = [], renderer;
+
+// @description: A Function that generates a PDF 
+function generatePDF() {
+    console.log("generating PDF - PDFを作成中");
+    const strMime = 'image/jpeg';
+      var doc = new jsPDF();
+      var imgData = this.renderer.domElement.toDataURL(strMime);
+      imgData.replace(strMime, this.strDownloadMime);
+      doc.addImage(imgData, 'JPEG', 5, -10, 150, 150);
+      doc.save("test.pdf");
+}
